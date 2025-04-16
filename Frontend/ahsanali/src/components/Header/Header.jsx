e4fedeafe603380bc8051bbdccf07f3ea24406e2
ahsanali2009 <ahsanali19112009@gmail.com>
2025-04-16 13:49:44 +0500

    icons

--- Frontend/ahsanali/src/components/Header/Header.jsx
+++ Frontend/ahsanali/src/components/Header/Header.jsx
@@ -17,6 +17,13 @@ export default function Header(){
                 <div className={styles.headerBoxDetailsII}>
                     <h1>ahsan.</h1>
                     <h3>Figuring Things Out.</h3>
+
+                    <div className={styles.headerBoxDetailsIISocials}>
+                        <ul>
+                            <li><a href="https://github.com/ahsanali2009" target="_blank"><img src="/src/assets/github.png" alt="github" /></a></li> 
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </div>
