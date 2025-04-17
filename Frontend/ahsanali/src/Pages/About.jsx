2e15a5ec32684400c38cef801979a014588706ce
ahsanali2009 <ahsanali19112009@gmail.com>
2025-04-17 11:32:33 +0500

    header issue fix

--- Frontend/ahsanali/src/Pages/About.jsx
+++ Frontend/ahsanali/src/Pages/About.jsx
@@ -6,15 +6,16 @@ export default function About(){
         <>
         
         <Header/>
-        
+
+
         <div className={styles.aboutPageContainer}>
 
         <div className={styles.aboutPageContainerContent}>
-            <pre>
-            I'm currently learning programming with a focus on <b>machine learning, artificial intelligence, computer science, and mathematics.</b><br />I'm working on a few personal projects while developing my skills in the MERN stack. 
-            My primary languages are JavaScript and Python, and I'm continuously exploring <br />how to apply them to build thoughtful, well-structured solutions. <br />
+            <p>
+            I'm currently learning programming with a focus on <b>machine learning, artificial intelligence, computer science, and mathematics.</b>I'm working on a few personal projects while developing my skills in the MERN stack. 
+            My primary languages are JavaScript and Python, and I'm continuously exploring how to apply them to build thoughtful, well-structured solutions.
             If you're interested in collaborating or hiring me for a project, feel free to reach out at <a href="mailto:ahsanali19112009@gmail.com"> ahsanali19112009@gmail.com</a>.
-            </pre>
+            </p>
         </div>
 
         </div>
