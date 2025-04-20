46568d3a78167bd8779c367a86526c255e313f0d
ahsanali2009 <ahsanali19112009@gmail.com>
2025-04-20 18:34:01 +0500

    api connection

--- Frontend/ahsanali/src/Pages/Home.jsx
+++ Frontend/ahsanali/src/Pages/Home.jsx
@@ -1,7 +1,18 @@
 import Header from '/src/components/Header/Header.jsx'
-
+import { useEffect } from 'react'
 
 export default function Home(){
+
+    useEffect(() => {
+        async function fetchBlogData(){
+            console.log("Working api")
+            let data = await fetch("http://localhost:8000/")
+            console.log(data)
+        }
+        fetchBlogData()
+    })
+
+
     return(
         <>
         
