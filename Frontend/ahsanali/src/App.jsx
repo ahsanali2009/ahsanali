1e18ff7ceefbe73914afcbf37f605a9d3c7ea2db
ahsanali2009 <ahsanali19112009@gmail.com>
2025-05-03 20:57:34 +0500

    admin panel route

--- Frontend/ahsanali/src/App.jsx
+++ Frontend/ahsanali/src/App.jsx
@@ -3,6 +3,7 @@ import About from './Pages/About.jsx'
 import Home from './Pages/Home.jsx'
 import NotFound from './Pages/NotFoundPage.jsx'
 import AuthPage from "./Pages/admin/AuthPage/Auth.jsx"
+import AdminPanel from "./Pages/admin/Admin Panel/AdminPanel.jsx"
 
 export default function App() {
 
@@ -14,6 +15,7 @@ return (
       <Route path="/about" element={<About/>}/>
       <Route path="*" element={<NotFound/>}/>
       <Route path="/admin" element={<AuthPage/>}/>
+      <Route path='/admin/:path' element={<AdminPanel/>}/>
     </Routes>
 
     </>
