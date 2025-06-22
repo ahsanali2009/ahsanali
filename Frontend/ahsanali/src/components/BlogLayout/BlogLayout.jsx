49b42b98ddc6bdda7643e132187eb41ab200975b
ahsanali2009 <ahsanali19112009@gmail.com>
2025-06-22 19:21:49 +0500

    blog path as slug

--- Frontend/ahsanali/src/components/BlogLayout/BlogLayout.jsx
+++ Frontend/ahsanali/src/components/BlogLayout/BlogLayout.jsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react'
 import styles from './BlogLayout.module.css'
+import { getPathOfBlogClicked } from '../../utils/getPath_BlogClicked'
+import { Link } from 'react-router-dom'
 
 export default function BlogLayout(props){
 
-    // useEffect(() => {
-    //     console.log(props)
-
-    // }, [])
-
-
+    
     // props['object']['title']
 
     return(
         <>
+
         <div className={styles.blogLayoutDiv}>
+        <a href={`/blog/${getPathOfBlogClicked(props['object']['path'])}`} className={styles.blogLayoutDivTitleahref}>
             <div className={styles.blogLayoutDivTitle}>
                 <h3>{props['object']['title']}</h3>
             </div>
+        </a>
             <div className={styles.blogLayoutDivContent}>
                 <p>{props['object']['blog_content']}</p>
             </div>
