5492b541fb7e1e589778da205f88c4e48d2fe5f4
Ahsan Ali <ahsanali19112009@gmail.com>
2025-11-02 18:29:54 +0500

    change

--- Frontend/ahsanali/src/Pages/About.jsx
+++ Frontend/ahsanali/src/Pages/About.jsx
@@ -9,9 +9,7 @@ export default function About(){
     <div className={styles.aboutPageContainer}>
         <div className={styles.aboutPageContainerContent}>
             <p>
-            I'm currently learning programming with a focus on <b>machine learning, artificial intelligence, computer science, and mathematics.</b>I'm working on a few personal projects while developing my skills in the MERN stack. 
-            My primary languages are JavaScript and Python, and I'm continuously exploring how to apply them to build thoughtful, well-structured solutions.
-            If you're interested in collaborating or hiring me for a project, feel free to reach out at <a href="mailto:ahsanali19112009@gmail.com"> ahsanali19112009@gmail.com</a>.
+
             </p>
             <a href="https://github.com/ahsanali2009" target='_blank'>Github</a>
         </div>
